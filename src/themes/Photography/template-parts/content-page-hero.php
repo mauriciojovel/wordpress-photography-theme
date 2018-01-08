4955d5ff3e00c299a1627bf0087441c3781b01ca
Mauricio Jovel <mjovel@applaudostudios.com>
2018-01-08 17:21:56 -0600

    Adding support to load photography posts.

---
+++ src/themes/Photography/template-parts/content-page-hero.php
@@ -0,0 +1,26 @@
+
+<div class="carousel-item <?php echo $active ?> <?php echo ( get_field('is_portratit') == 'true' ? 'portrait' : '' ) ?>" style="background-image: url('<?php echo get_the_post_thumbnail_url(null, 'full'); ?>')">
+    <div class="carousel-caption hero">
+        <div class="container pb-4 pb-lg-3">
+            <div class="row align-items-center">
+                <div class="col-sm-12 col-lg-3 text-center p-1">
+                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 75, null, false, array('class' => 'profile-img img-thumbnail rounded-circle' ) ); ?>
+                </div>
+                <div class="col-sm-12 col-lg-6 photo-text p-1 d-none d-lg-block">
+                    <?php the_content(); ?>
+                </div>
+                <div class="col-sm-12 col-lg-3 photo-text p-1">
+                    <?php $imageLocation =  get_attached_file( get_field('_thumbnail_id') ); ?>
+                    <?php $exif = exif_read_data( $imageLocation, 'EXIF' ); ?>
+                        <b><?php echo $exif['Model'] ?></b>
+                        <br />
+                        f/<?php echo  eval('return '.$exif['FNumber'].';') ?>
+                        <?php $time = eval( 'return '. $exif['ExposureTime'] . ';'); ?>
+                        <?php echo ( $time < 1 ? $exif['ExposureTime'] : $time ) ?>''
+                        <?php echo eval('return '.$exif['FocalLength'].';') ?>mm
+                        ISO <?php echo $exif['ISOSpeedRatings'] ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/themes/Photography/templates/hero-home.php
+++ src/themes/Photography/templates/hero-home.php
@@ -6,79 +6,100 @@
     filemtime(get_template_directory() . '/assets/css/home.css') 
     ); 
 ?>
+
 <?php get_header(); ?>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active" style="background-image: url('http://lorempixel.com/1920/1096/nature/1/')">
-                <div class="carousel-caption hero">
-                    <div class="container pb-4 pb-lg-3">
-                        <div class="row align-items-center">
-                            <div class="col-sm-12 col-lg-3 text-center p-1">
-                                <img src="assets/img/profile2.jpg" class="profile-img img-thumbnail rounded-circle"/>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 photo-text p-1 d-none d-lg-block">
-                                    Ut eu ante lacus. Vestibulum egestas blandit massa, sit amet pretium libero. Sed rhoncus orci vel viverra lacinia. Cras pharetra, odio in sagittis dignissim, dui nisi molestie orci, nec molestie diam libero sit amet est. Aliquam volutpat turpis duis.                                    
-                            </div>
-                            <div class="col-sm-12 col-lg-3 photo-text p-1">
-                                    <b>NIKON D5500</b>
-                                    <br />
-                                    f/2.8 1/250 85mm ISO 1600
-                            </div>
-                        </div>
+
+<?php 
+    $weeks = get_categories( array(
+        'taxonomy' => 'week',
+        'hide_empty' => 0,
+        'orderby' => 'term_id',
+        'order' => 'DESC'
+     ) );
+    
+    $lastWeek = null;
+    $preLastWeek = null;
+    $term = null;
+
+    $totalWeeks = count($weeks); 
+    if( $totalWeeks > 0) {
+        $lastWeek = $weeks[0];
+        $term = $lastWeek->slug;
+        if( $totalWeeks > 1 ) {
+            $preLastWeek = $weeks[1];
+            $term = $preLastWeek->slug;
+        }
+    }
+
+    if($term != null) {
+        $args = array(
+            'post_type' => 'photography',
+            'post_status' => 'publish',
+            'orderby' => 'publish_date',
+            'order' => 'DESC',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'week',
+                    'field'    => 'slug',
+                    'terms'    => $term,
+                ),
+            ),
+            'meta_key'		=> 'is_feature',
+            'meta_value'	=> true
+        );
+        
+        $query = new WP_Query( $args );
+
+        if( !$query->have_posts() ) {
+            $args = array(
+                'post_type' => 'photography',
+                'post_status' => 'publish',
+                'orderby' => 'publish_date',
+                'order' => 'DESC',
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'week',
+                        'field'    => 'slug',
+                        'terms'    => $lastWeek->slug,
+                    ),
+                ),
+            );
+            $query = new WP_Query( $args );
+        }
+
+        $i = 0;
+        $count = $query->post_count;
+    }
+?>
+
+<?php if ( isset ( $query ) && $query->have_posts() ) : ?>
+<?php   while ( $query->have_posts() ) : $query->the_post(); ?>
+            <?php set_query_var( 'active', ( $i == 0 ? 'active' : '' ) ); ?>
+            <?php if ( $i == 0 ) : ?>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+                    <ol class="carousel-indicators">
+                        <?php for($j = 0 ; $j < $count ; $j++ ) : ?>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $j ?>" class="<?php echo ( $j == 0 ? 'active': '' ) ?>"></li>
+                        <?php endfor; ?>
+                    </ol>
+                    <div class="carousel-inner">
+            <?php endif; ?>
+<?php       get_template_part( 'template-parts/content', 'page-hero' ); ?>
+            <?php if ( $i == $count - 1 ) : ?> 
                     </div>
-                </div>
-            </div>
-            <div class="carousel-item" style="background-image: url('http://lorempixel.com/1920/1096/nature/2/')">
-                <div class="carousel-caption hero">
-                    <div class="container pb-4 pb-lg-3">
-                        <div class="row align-items-center">
-                            <div class="col-sm-12 col-lg-3 text-center p-1">
-                                <img src="assets/img/profile2.jpg" class="profile-img img-thumbnail rounded-circle"/>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 photo-text p-1 d-none d-lg-block">
-                                    Ut eu ante lacus. Vestibulum egestas blandit massa, sit amet pretium libero. Sed rhoncus orci vel viverra lacinia. Cras pharetra, odio in sagittis dignissim, dui nisi molestie orci, nec molestie diam libero sit amet est. Aliquam volutpat turpis duis.                                    
-                            </div>
-                            <div class="col-sm-12 col-lg-3 photo-text p-1">
-                                    <b>NIKON D5500</b>
-                                    <br />
-                                    f/2.8 1/250 85mm ISO 1600
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item portrait" style="background-image: url('http://lorempixel.com/1096/1920/nature/3/')">
-                <div class="carousel-caption hero">
-                    <div class="container pb-4 pb-lg-3">
-                        <div class="row align-items-center">
-                            <div class="col-sm-12 col-lg-3 text-center p-1">
-                                <img src="assets/img/profile.png" class="profile-img img-thumbnail rounded-circle"/>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 photo-text p-1 d-none d-lg-block">
-                                    Ut eu ante lacus. Vestibulum egestas blandit massa, sit amet pretium libero. Sed rhoncus orci vel viverra lacinia. Cras pharetra, odio in sagittis dignissim, dui nisi molestie orci, nec molestie diam libero sit amet est. Aliquam volutpat turpis duis.                                    
-                            </div>
-                            <div class="col-sm-12 col-lg-3 photo-text p-1">
-                                    <b>NIKON D5500</b>
-                                    <br />
-                                    f/2.8 1/250 85mm ISO 1600
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>  
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>      
+            <?php endif; ?>
+            <?php $i++ ?>
+<?php   endwhile; ?>
+
+<?php   wp_reset_postdata(); ?>
+<?php endif; ?>
 <?php get_footer(); ?>
