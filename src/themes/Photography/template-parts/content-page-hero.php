fd3ff2332163565a48fade86c80f855cce8f1dd3
Mauricio Jovel <mjovel@applaudostudios.com>
2018-01-10 11:50:49 -0600

    Change the function to fetch the author image

--- src/themes/Photography/template-parts/content-page-hero.php
+++ src/themes/Photography/template-parts/content-page-hero.php
@@ -4,7 +4,7 @@
         <div class="container pb-4 pb-lg-3">
             <div class="row align-items-center">
                 <div class="col-sm-12 col-lg-3 text-center p-1">
-                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 75, '', false, array('class' => array( 'profile-img', 'img-thumbnail', 'rounded-circle' ) ) ); ?>
+                    <img alt="" src="<?php echo get_avatar( get_the_author_meta( 'ID' ),  array( 'size' => 75, 'default' => 'mysteryman' ) ); ?>" class="avatar avatar-wordpress-social-login avatar-75 photo profile-img img-thumbnail rounded-circle" height="75" width="75">
                 </div>
                 <div class="col-sm-12 col-lg-6 photo-text p-1 d-none d-lg-block">
                     <?php the_content(); ?>
