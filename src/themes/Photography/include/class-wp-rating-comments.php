cdd311c87bdc3437c973241b67243e2d9ac2a24b
Mauricio Jovel <mjovel@applaudostudios.com>
2018-01-30 15:39:51 -0600

    Adding edit comment rating to the admin panel

--- src/themes/Photography/include/class-wp-rating-comments.php
+++ src/themes/Photography/include/class-wp-rating-comments.php
@@ -9,6 +9,8 @@ class Rating_Comments {
         add_action( 'comment_form_logged_in_after', array( $this,'additional_fields' ) );
         add_action( 'comment_form_after_fields', array( $this,'additional_fields' ) );
         add_action( 'comment_post', array( $this, 'save_comment_meta_data' ) );
+        add_action( 'add_meta_boxes_comment', array( $this, 'comment_add_meta_box' ) );
+        add_action( 'edit_comment', array( $this, 'comment_edit_comment' ) );
     }
 
     function additional_fields () {
@@ -34,5 +36,41 @@ class Rating_Comments {
             $rating = '';
         }
         add_comment_meta( $comment_id, 'rating', $rating );
+    }
+
+    /**
+     * Add the title to our admin area, for editing, etc
+     */
+    
+    function comment_add_meta_box()
+    {
+        add_meta_box( 'pmg-comment-title', __( 'Rating', 'photography' ), array($this,'comment_meta_box_cb'), 'comment', 'normal', 'high' );
+    }
+
+    function comment_meta_box_cb( $comment )
+    {
+        $rating = get_comment_meta( $comment->comment_ID, 'rating', true );
+        wp_nonce_field( 'comment_update', 'comment_update', false );
+        ?>
+        <p>
+            <label for="rating"><?php _e( 'Rating', 'photography' ); ?></label>
+            <select name="rating" class="widefat">
+                <option value="" <?php if( $rating == '' ) { echo 'selected'; } ?> ><?php echo __('No rating', 'photography') ?></option>
+                <option value="1" <?php if( $rating == '1' ) { echo 'selected'; } ?> >1</option>
+                <option value="2" <?php if( $rating == '2' ) { echo 'selected'; } ?> >2</option>
+                <option value="3" <?php if( $rating == '3' ) { echo 'selected'; } ?> >3</option>
+            </select>
+        </p>
+        <?php
+    }
+    /**
+     * Save our comment (from the admin area)
+     */
+    
+    function comment_edit_comment( $comment_id )
+    {
+        if( ! isset( $_POST['comment_update'] ) || ! wp_verify_nonce( $_POST['comment_update'], 'comment_update' ) ) return;
+        if( isset( $_POST['rating'] ) )
+            update_comment_meta( $comment_id, 'rating', esc_attr( $_POST['rating'] ) );
     }
 }
